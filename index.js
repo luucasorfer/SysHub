1fd6b73cbfaf7e6ecb0eb4db389d45803e919977
luucasorfer <lucas.fernandes.94.lf@gmail.com>
2023-04-03 12:43:44 -0300

    Update de projeto
    * Adicionado CSS para impressão da lista telefônica;
    * Adição de comentários no código;
    *Adição do caminho do CSS via JS.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// Importa os módulos das páginas
 import home from "../src/templates/home/index.js";
 import phoneList from "../src/templates/phoneList/index.js";
 import keyuser from "../src/templates/keyuser/index.js";
 import ccusto from "../src/templates/ccusto/index.js";
 import bip from "../src/templates/bip/index.js";
 
+// Seleciona o elemento principal onde as páginas serão renderizadas
 const main = document.querySelector("#root");
 
+// Cria um objeto com as rotas e as funções correspondentes às páginas
 const routes = {
   "#home": home,
   "#phoneList": phoneList,
@@ -14,47 +17,73 @@ const routes = {
   "#bip": bip,
 };
 
+// Função que inicializa a página
 const init = () => {
+  // Obtém a rota atual da URL
   const currentRoute = window.location.hash;
+  // Limpa o conteúdo do elemento principal
   main.innerHTML = "";
+  // Adiciona a página correspondente à rota atual no elemento principal
   main.appendChild(routes[currentRoute]());
 
-  const cssLink = document.createElement("link");
-  cssLink.rel = "stylesheet";
+  // Cria um elemento link para o arquivo CSS correspondente à rota atual
+  const cssStyle = document.createElement("link");
+  cssStyle.rel = "stylesheet";
 
+  const cssPrint = document.createElement("link");
+  cssPrint.rel = "stylesheet";
+
+  // Cria um elemento script para o arquivo JavaScript correspondente à rota atual
   const script = document.createElement("script");
 
+  // Verifica a rota atual e define o caminho dos arquivos CSS e JS correspondentes
   switch (currentRoute) {
     case "#phoneList":
-      cssLink.href = "../src/templates/phoneList/stylePhoneList.css";
+      cssStyle.href = "../src/templates/phoneList/stylePhoneList.css";
+      cssPrint.href = "../src/templates/phoneList/print.css";
       script.src = "../src/templates/phoneList/script.js";
       break;
     default:
-      cssLink.href = `../src/templates/${currentRoute.slice(1)}/style.css`;
+      cssStyle.href = `../src/templates/${currentRoute.slice(1)}/style.css`;
+      cssPrint.href = `../src/templates/${currentRoute.slice(1)}/style.css`;
       script.src = `../src/templates/${currentRoute.slice(1)}/script.js`;
       break;
   }
 
-  document.querySelector("head").appendChild(cssLink);
+  // Adiciona o elemento link no cabeçalho da página
+  document.querySelector("head").appendChild(cssStyle);
+  document.querySelector("head").appendChild(cssPrint);
+
+  // Adiciona o elemento script no final do body da página
   document.body.appendChild(script);
 };
 
+// Evento que é disparado quando a página é carregada
 window.addEventListener("load", () => {
+  // Inicializa a página
   init();
 });
 
+// Evento que é disparado quando a rota na URL é alterada
 window.addEventListener("hashchange", () => {
+  // Inicializa a página
   init();
 });
 
+// Evento que é disparado antes da página ser fechada
 window.addEventListener("beforeunload", () => {
+  // Obtém a rota atual da URL
   const currentRoute = window.location.hash;
+  // Salva a rota atual no localStorage
   localStorage.setItem("lastRoute", currentRoute);
 });
 
+// Evento que é disparado quando o DOM é carregado
 window.addEventListener("DOMContentLoaded", () => {
+  // Obtém a última rota salva no localStorage
   const lastRoute = localStorage.getItem("lastRoute");
   if (lastRoute) {
+    // Redireciona para a última rota salva no localStorage
     window.location.hash = lastRoute;
   }
 });
